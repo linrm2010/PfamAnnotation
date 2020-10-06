a4a03a870189cbc16c9fb5896a4519e53bee78c6
linrm2010 <66455878+linrm2010@users.noreply.github.com>
2020-10-06 15:11:08 +0800

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -1,10 +1,3 @@
-chmod +x PfamAnnotation
-chmod +x ObtainPfamAStat
-chmod +x CheckPfamStatRepeat
-chmod +x FilterPfamStatEvalue
-chmod +x DomainListForGene
-chmod +x PfamFamilyStat
-
 mkdir ../bin/
 cp PfamAnnotation ../bin/
 cp ObtainPfamAStat ../bin/
@@ -12,3 +5,11 @@ cp CheckPfamStatRepeat ../bin/
 cp FilterPfamStatEvalue ../bin/
 cp DomainListForGene ../bin/
 cp PfamFamilyStat ../bin/
+
+cd ../bin/
+chmod +x PfamAnnotation
+chmod +x ObtainPfamAStat
+chmod +x CheckPfamStatRepeat
+chmod +x FilterPfamStatEvalue
+chmod +x DomainListForGene
+chmod +x PfamFamilyStat
