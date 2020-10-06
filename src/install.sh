2a97ab3602226d1eb7acb2d10186e767f5b92387
linrm2010 <66455878+linrm2010@users.noreply.github.com>
2020-10-06 15:10:13 +0800

    Create install.sh

---
+++ src/install.sh
@@ -0,0 +1,14 @@
+chmod +x PfamAnnotation
+chmod +x ObtainPfamAStat
+chmod +x CheckPfamStatRepeat
+chmod +x FilterPfamStatEvalue
+chmod +x DomainListForGene
+chmod +x PfamFamilyStat
+
+mkdir ../bin/
+cp PfamAnnotation ../bin/
+cp ObtainPfamAStat ../bin/
+cp CheckPfamStatRepeat ../bin/
+cp FilterPfamStatEvalue ../bin/
+cp DomainListForGene ../bin/
+cp PfamFamilyStat ../bin/
